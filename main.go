bfbece67501b468ed2bb33b993fc34b69f67da36
leon <leiyang_ace@163.com>
2020-03-22 12:05:25 +0800

    添加对本机资源地址返回

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/autotls"
 	"github.com/gin-gonic/gin"
 	"io"
+	"io/ioutil"
 	"log"
 	"os"
 )
@@ -25,6 +27,7 @@ func main() {
 
 	r.LoadHTMLFiles("templates/help.html")
 
+	r.GET("/assert", AssertsView)
 	r.GET("/random", RandomView)
 	r.GET("/help", func(c *gin.Context) {
 		c.HTML(200, "help.html", gin.H{
@@ -33,7 +36,7 @@ func main() {
 	})
 	go TickClearCache()
 
-	log.Fatal(r.Run(":1627"))
+	log.Fatal(autotls.Run(r, "assert.freaks.group"))
 }
 
 func RandomView(c *gin.Context) {
@@ -48,6 +51,48 @@ func RandomView(c *gin.Context) {
 
 	c.String(statusCode, res)
 
+}
+
+// 本网站的资源
+type AssertParam struct {
+	Category string `form:"category" binding:"required"`
+	Tag      string `form:"tag" binding:"required"`
+}
+
+func AssertsView(c *gin.Context) {
+	var param AssertParam
+	var basePath = "/home/asserts"
+
+	if err := c.ShouldBind(&param); err != nil {
+		c.String(400, "error: %s", err)
+		c.Abort()
+		return
+	}
+	aCategory := c.Query("category")
+	aTag := c.Query("tag")
+
+	assertPath := basePath + "/" + aCategory + "/" + aTag
+	rd, err := ioutil.ReadDir(assertPath)
+	if err != nil {
+		fmt.Println("资源路径不存在")
+		c.JSON(404, gin.H{
+			"code": 404,
+			"msg":  "资源路径不存在",
+		})
+	}
+
+	var data []string
+	urlBasePath := "https://asserts.freaks.group" + "/" + aCategory + "/" + aTag
+	for _, fi := range rd {
+		fmt.Println(fi.Name())
+		data = append(data, fi.Name())
+	}
+	c.JSON(200, gin.H{
+		"code":        200,
+		"msg":         "success",
+		"data":        data,
+		"urlBasePath": urlBasePath,
+	})
 }
 
 // 参数格式
